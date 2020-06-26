e02aa4fee6dc9970c8b2902454062dd2878a48f2
caplight <378603374@qq.com>
2020-06-26 16:37:03 +0800

    增加系统管理模块集群 完善功能 全局日志 数据库备份 6/26 from caplight

---
+++ cloud-gateway8302/src/main/java/com/cloud/gateway/sentinel/config/GatewayConfiguration.java
@@ -0,0 +1,69 @@
+package com.cloud.gateway.sentinel.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import com.cloud.gateway.sentinel.handler.JsonSentinelGatewayBlockExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author CAPLIGHT
+ * @create 2020-06-24 3:55 PM
+ */
+@Configuration
+public class GatewayConfiguration {
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    /**
+     * 配置SentinelGatewayBlockExceptionHandler，限流后异常处理
+     * @return
+     */
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public JsonSentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        return new JsonSentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    /**
+     * 配置SentinelGatewayFilter
+     * @return
+     */
+    @Bean
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+    @PostConstruct
+    public void doInit() {
+        initGatewayRules();
+    }
+
+    /**
+     * 配置限流规则
+     */
+    private void initGatewayRules() {
+
+    }
+}
